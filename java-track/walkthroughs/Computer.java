acd50cf1f6ac0fe33ec7905ef810a2d1f803f1b7
Ji Park <jiwpark00@gmail.com>
2016-10-03 16:52:27 -0500

    Following Doug's tutorial.

---
+++ java-track/walkthroughs/Computer.java
@@ -0,0 +1,20 @@
+
+public class Computer {
+	
+	// Fields:
+	
+	// memory - int
+	// size - double
+	// processor - double
+	// brand - String
+	
+	// Behaviors
+	
+	// Create a computer (nearly every class we create has a constructor.
+	// Get the brand name
+	// Get the size
+	// Get the processing speed
+	// Add memory
+	// Swap out the processor
+	// Display information as a String
+}
