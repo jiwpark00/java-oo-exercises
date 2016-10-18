187863de3c20b07c3d52a14a0763950c67b5428d
Ji Park <jiwpark00@gmail.com>
2016-10-17 19:55:53 -0500

    Followed Doug's tutorial for introducing polymorphism.

--- java-track/walkthroughs/Laptop.java
+++ java-track/walkthroughs/Laptop.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Laptop extends Computer {
 	
@@ -15,4 +16,21 @@ public class Laptop extends Computer {
 	public void open() {
 		this.isClosed = false;
 	}
+	
+	public static void main(String args[]) {
+		Laptop l = new Laptop(8,15,100000,"Apple");
+		Tablet t = new Tablet(4,10,1000,"Lenovo");
+		Computer c = new Computer(2,12,100,"HP");
+		
+		System.out.println(l.getBrand());
+		System.out.println(t.getBrand());
+		System.out.println(c.getBrand());
+		
+		ArrayList<Computer> computers = new ArrayList<Computer>();
+		computers.add(c);
+		computers.add(l);
+		computers.add(t);
+		
+	}
+	
 }
