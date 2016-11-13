e8c3a092f8891beef631845011f0c69f56f0dc74
Ji Park <jiwpark00@gmail.com>
2016-11-13 12:08:38 -0600

    Finished watching videos for Thymeleaf.

--- hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.launchcode.models.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,18 +13,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(HttpServletRequest request) {
+	public String helloForm() {
+		return "helloform";
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	//@ResponseBody Now getting rid of this because that's what allowed to just print a string
+	public String hello(HttpServletRequest request, Model model) {
 		
 		// get name parameter from request; will be null when no parameter is passed
 		// so let's fix that 
+		
 		String name = request.getParameter("name");
 		
-		if (name == null) {
+		if (name == null || name == "") {
 			name = "world";
 		}
 		
-		return "<h1>" + HelloMessage.getMessage(name) + "</h1>";
+		model.addAttribute("message",HelloMessage.getMessage(name));
+		model.addAttribute("title", "Hello, Spring! Resonse");
+		return "hello";
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
